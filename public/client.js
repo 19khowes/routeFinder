b14d11f414b8ca2f92e68893baeb91f23bef3c79
19khowes <72945859+19khowes@users.noreply.github.com>
2021-08-16 15:31:06 -0600

    Start of drag and drop

--- public/client.js
+++ public/client.js
@@ -42,3 +42,35 @@ submitButton.addEventListener('click', async () => {
     // newmarker.bindPopup(label);
     // // // console.log(latInput.value, lonInput.value);
 });
+
+function dropHandler(ev) {
+    console.log('File(s) dropped');
+
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+
+    if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+            // If dropped items aren't files, reject them
+            if (ev.dataTransfer.items[i].kind === 'file') {
+                var file = ev.dataTransfer.items[i].getAsString((data) => {
+                    console.log(data);
+                });
+            }
+        }
+    } else {
+        // Use DataTransfer interface to access the file(s)
+        for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+            console.log('file: ' + ev.dataTransfer.files[i]);
+        }
+    }
+}
+
+function dragOverHandler(ev) {
+    console.log('File(s) in drop zone');
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  }
+  
